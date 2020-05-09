839b82f41ac370a3039fdd925314f13ac3332b7c
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-05-09 21:58:39 +0300

    Add project files.

---
+++ ScoopBox/ConfigurationEntities/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace ScoopBox.ConfigurationEntities
+{
+    [XmlRoot(ElementName = "Configuration")]
+    public class Configuration
+    {
+        [XmlElement(ElementName = "VGpu")]
+        public string VGpu { get; set; }
+
+        [XmlElement(ElementName = "Networking")]
+        public string Networking { get; set; }
+
+        [XmlElement(ElementName = "LogonCommand")]
+        public LogonCommand LogonCommand { get; set; }
+    }
+}
---
+++ ScoopBox/ConfigurationEntities/LogonCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace ScoopBox.ConfigurationEntities
+{
+    [XmlRoot(ElementName = nameof(LogonCommand))]
+    public class LogonCommand
+    {
+        [XmlElement(ElementName = nameof(Command))]
+        public string Command { get; set; }
+    }
+}
