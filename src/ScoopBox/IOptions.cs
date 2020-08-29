33bdf82851edfc8191043875fdc1d71fb82c2c27
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-08-29 16:05:34 +0300

    Add new options class to further customize the library.

---
+++ src/ScoopBox/IOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace ScoopBox
+{
+    public interface IOptions
+    {
+        string SandboxBeforeScriptsLocation { get; }
+
+        string SandboxAfterScriptsLocation { get; }
+
+        string SandboxPackageManagerScriptsLocation { get; }
+
+        string RootFilesDirectoryLocation { get; }
+
+        string SandboxConfigurationFileName { get; }
+    }
+}
---
+++ src/ScoopBox/Options.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ScoopBox
+{
+    public class Options : IOptions
+    {
+        public Options()
+            : this(
+                  @"C:\Users\WDAGUtilityAccount\Desktop\Sandbox\BeforeScripts",
+                  @"C:\Users\WDAGUtilityAccount\Desktop\Sandbox\AfterScripts",
+                  @"C:\Users\WDAGUtilityAccount\Desktop\Sandbox\PackageManagerScripts",
+                  "sandbox.wsb",
+                  Directory.CreateDirectory($"{Path.GetTempPath()}/{Constants.SandboxFolderName}").FullName)
+        {
+        }
+
+        public Options(
+            string sandboxBeforeScriptsLocation,
+            string sandboxAfterScriptsLocation,
+            string sandboxPackageManagerScriptsLocation,
+            string sandboxConfigurationFileName,
+            string rootFilesDirectoryLocation)
+        {
+            this.SandboxBeforeScriptsLocation = sandboxBeforeScriptsLocation ?? throw new ArgumentNullException(nameof(sandboxBeforeScriptsLocation));
+            this.SandboxAfterScriptsLocation = sandboxAfterScriptsLocation ?? throw new ArgumentNullException(nameof(sandboxAfterScriptsLocation));
+            this.SandboxPackageManagerScriptsLocation = sandboxPackageManagerScriptsLocation ?? throw new ArgumentNullException(nameof(sandboxPackageManagerScriptsLocation));
+            this.SandboxConfigurationFileName = sandboxConfigurationFileName ?? throw new ArgumentNullException(nameof(sandboxConfigurationFileName));
+            this.RootFilesDirectoryLocation = rootFilesDirectoryLocation ?? throw new ArgumentNullException(nameof(rootFilesDirectoryLocation));
+        }
+
+        public string SandboxBeforeScriptsLocation { get; }
+
+        public string SandboxAfterScriptsLocation { get; }
+
+        public string SandboxPackageManagerScriptsLocation { get; }
+
+        public string SandboxConfigurationFileName { get; }
+
+        public string RootFilesDirectoryLocation { get; }
+    }
+}
