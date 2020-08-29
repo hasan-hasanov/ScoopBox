a968b70efc3774a3f2bcf694176db6b5628d131d
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-08-29 17:51:49 +0300

    Introduce command builders to execute scripts inside sandbox.

---
+++ src/ScoopBox/CommandBuilders/ICommandBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace ScoopBox.CommandBuilders
+{
+    public interface ICommandBuilder
+    {
+        string Build(string fullScriptName);
+
+        string Build(string fullScriptName, string[] argumentsBeforeScript, string[] argumentsAfterScript);
+    }
+}
---
+++ src/ScoopBox/CommandBuilders/PowershellCommandBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace ScoopBox.CommandBuilders
+{
+    public class PowershellCommandBuilder : ICommandBuilder
+    {
+        public string Build(string fullScriptName)
+        {
+            return Build(fullScriptName, null, null);
+        }
+
+        public string Build(string fullScriptName, string[] argumentsBeforeScript, string[] argumentsAfterScript)
+        {
+            if (string.IsNullOrWhiteSpace(fullScriptName))
+            {
+                throw new ArgumentNullException(nameof(fullScriptName));
+            }
+
+            StringBuilder sbPowershellCommandBuilder = new StringBuilder();
+            sbPowershellCommandBuilder
+                .Append("powershell.exe")
+                .Append(" ");
+
+            if (argumentsBeforeScript?.Length > 0)
+            {
+                string beforeArguments = string.Join(" ", argumentsBeforeScript);
+
+                sbPowershellCommandBuilder
+                    .Append(beforeArguments)
+                    .Append(" ");
+            }
+
+            sbPowershellCommandBuilder
+                .Append($"{fullScriptName}");
+
+            if (argumentsAfterScript?.Length > 0)
+            {
+                string afterArguments = string.Join(" ", argumentsAfterScript);
+
+                sbPowershellCommandBuilder
+                    .Append(afterArguments);
+            }
+
+            return sbPowershellCommandBuilder.ToString();
+        }
+    }
+}
