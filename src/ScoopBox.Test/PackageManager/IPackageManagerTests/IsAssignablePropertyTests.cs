fd2c554d7beebbed48439c1f72ef6117a63736b3
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-08-30 23:01:32 +0300

    Add assignability tests from package manager.

---
+++ src/ScoopBox.Test/PackageManager/IPackageManagerTests/IsAssignablePropertyTests.cs
@@ -0,0 +1,18 @@
+﻿using ScoopBox.PackageManager;
+using ScoopBox.PackageManager.Scoop;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ScoopBox.Test.PackageManager.IPackageManagerTests
+{
+    public class IsAssignablePropertyTests
+    {
+        [Fact]
+        public void IsCmdCommandBuilderAssignableToICommandBuilder()
+        {
+            ScoopPackageManager scoopPackageManager = new ScoopPackageManager(new List<string>() { "git" });
+
+            Assert.IsAssignableFrom<IPackageManager>(scoopPackageManager);
+        }
+    }
+}
