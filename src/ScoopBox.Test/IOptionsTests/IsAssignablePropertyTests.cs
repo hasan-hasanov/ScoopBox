65a4f66181299f6dd9479de4176e0ad0b0e95945
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-08-31 14:33:31 +0300

    Add all assignability tests.

---
+++ src/ScoopBox.Test/IOptionsTests/IsAssignablePropertyTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ScoopBox.Test.IOptionsTests
+{
+    public class IsAssignablePropertyTests
+    {
+        [Fact]
+        public void IsOptionsAssignableToIOptions()
+        {
+            Options options = new Options();
+
+            Assert.IsAssignableFrom<IOptions>(options);
+        }
+    }
+}
