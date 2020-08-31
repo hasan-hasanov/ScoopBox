47634049ba98d7125b3fb11e57de9affaa320b9f
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-08-31 14:28:34 +0300

    Add process assignability tests.

---
+++ src/ScoopBox.Test/SandboxProcesses/ISandboxProcessTests/IsAssignablePropertyTests.cs
@@ -0,0 +1,17 @@
+﻿using ScoopBox.SandboxProcesses;
+using ScoopBox.SandboxProcesses.Cmd;
+using Xunit;
+
+namespace ScoopBox.Test.SandboxProcesses.ISandboxProcessTests
+{
+    public class IsAssignablePropertyTests
+    {
+        [Fact]
+        public void IsSandboxProcessAssignableToSandboxCmdProcess()
+        {
+            SandboxCmdProcess sandboxCmdProcess = new SandboxCmdProcess();
+
+            Assert.IsAssignableFrom<ISandboxProcess>(sandboxCmdProcess);
+        }
+    }
+}
---
+++ src/ScoopBox.Test/SandboxProcesses/ProcessAdapters/IProcessAdapterTests/IsAssignablePropertyTests.cs
@@ -0,0 +1,16 @@
+﻿using ScoopBox.SandboxProcesses.ProcessAdapters;
+using Xunit;
+
+namespace ScoopBox.Test.SandboxProcesses.ProcessAdapters.IProcessAdapterTests
+{
+    public class IsAssignablePropertyTests
+    {
+        [Fact]
+        public void IsProcessAdapterAssignableToIProcessAdapter()
+        {
+            ProcessAdapter processAdapter = new ProcessAdapter("test");
+
+            Assert.IsAssignableFrom<IProcessAdapter>(processAdapter);
+        }
+    }
+}
