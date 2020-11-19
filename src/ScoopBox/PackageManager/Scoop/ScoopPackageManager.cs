f70528a2130b371e8896086edee93f920ee8c45e
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-11-20 00:06:33 +0200

    Implement scoop package manager

---
+++ src/ScoopBox/PackageManager/Scoop/ScoopPackageManager.cs
@@ -0,0 +1,57 @@
+﻿using ScoopBox.Scripts;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoopBox.PackageManager.Scoop
+{
+    public class ScoopPackageManager : IPackageManager, IScript
+    {
+        private string _packageManagerScriptName;
+        private readonly IFileSystem _fileSystem;
+        private readonly StringBuilder _sbScoopPackageManagerBuilder;
+
+        public ScoopPackageManager(IEnumerable<string> applications)
+            : this(applications, $"{nameof(ScoopPackageManager)}.ps1")
+        {
+        }
+
+        public ScoopPackageManager(IEnumerable<string> applications, string scriptName)
+            : this(applications, scriptName, new FileSystem())
+        {
+        }
+
+        private ScoopPackageManager(IEnumerable<string> applications, string scriptName, IFileSystem fileSystem)
+        {
+            _packageManagerScriptName = scriptName;
+            _fileSystem = fileSystem;
+
+            Applications = applications;
+            _sbScoopPackageManagerBuilder = new StringBuilder();
+        }
+
+        public FileSystemInfo ScriptFile { get; set; }
+
+        public IEnumerable<string> Applications { get; }
+
+        internal string Location { get; set; }
+
+        public async Task GenerateScript()
+        {
+            _sbScoopPackageManagerBuilder.AppendLine("Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')");
+            _sbScoopPackageManagerBuilder.AppendLine("scoop install git");
+            _sbScoopPackageManagerBuilder.AppendLine("scoop bucket add extras");
+            _sbScoopPackageManagerBuilder.Append("scoop install").Append(" ").AppendLine(string.Join(" ", Applications));
+
+            string fullScriptPath = Path.Combine(Location, _packageManagerScriptName);
+            using (StreamWriter writer = _fileSystem.File.CreateText(fullScriptPath))
+            {
+                await writer.WriteAsync(_sbScoopPackageManagerBuilder.ToString());
+            }
+
+            ScriptFile = new FileInfo(fullScriptPath);
+        }
+    }
+}
