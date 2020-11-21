52859417bea68e4c5e44b2207c5709b8dbc27a9d
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-11-21 17:10:47 +0200

    Add ISandbox tests

---
+++ src/ScoopBox.Test/ISandboxTests/IsAssignablePropertyTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ScoopBox.Test.ISandboxTests
+{
+    public class IsAssignablePropertyTests
+    {
+        [Fact]
+        public void ISandboxAssignableToSandbox()
+        {
+            Sandbox sandbox = new Sandbox();
+
+            Assert.IsAssignableFrom<ISandbox>(sandbox);
+        }
+    }
+}
