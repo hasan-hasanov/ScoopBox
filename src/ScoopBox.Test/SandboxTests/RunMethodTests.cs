4f74ff164d9d5ecb7741f10603b7562172a1535f
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-11-21 21:47:41 +0200

    Test sandbox run method

--- src/ScoopBox.Test/SandboxTests/RunMethodTests.cs
+++ src/ScoopBox.Test/SandboxTests/RunMethodTests.cs
@@ -1,4 +1,5 @@
-﻿using ScoopBox.SandboxConfigurations;
+﻿using Moq;
+using ScoopBox.SandboxConfigurations;
 using ScoopBox.Scripts;
 using ScoopBox.Scripts.Materialized;
 using ScoopBox.Scripts.PackageManagers.Scoop;
@@ -18,7 +19,7 @@ namespace ScoopBox.Test.SandboxTests
     public class RunMethodTests
     {
         [Fact]
-        public async Task ShouldBuilCorrectMainScriptContent()
+        public async Task ShouldBuildCorrectMainScriptContent()
         {
             IList<string> actualScripts = new List<string>();
 
@@ -80,6 +81,68 @@ namespace ScoopBox.Test.SandboxTests
             IList<string> actual = actualScripts;
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task ShouldCallSandboxConfigurationBuilderAndStartProcess()
+        {
+            int timesCalled = 0;
+
+            IOptions options = new Options();
+            Action<string> createDirectory = path => { };
+            Action<string> deleteFiles = path => { };
+            Action<string> deleteDirectories = path => { };
+            Func<string, Task> startProcess = path =>
+            {
+                timesCalled++;
+                return Task.CompletedTask;
+            };
+            Func<string, byte[], CancellationToken, Task> writeAllBytes = (path, content, token) => Task.CompletedTask;
+            Func<string, string, CancellationToken, Task> copyFiles = (source, destination, token) => Task.CompletedTask;
+            Func<IList<string>, IPowershellTranslator, string, IOptions, Task<LiteralScript>> literalScriptFactory = async (scripts, translator, name, options) =>
+            {
+                var literalScript = new LiteralScript(scripts, translator, name, deleteFiles, writeAllBytes);
+                await literalScript.Process(options);
+                return literalScript;
+            };
+
+            Mock<ISandboxConfigurationBuilder> configurationBuilder = new Mock<ISandboxConfigurationBuilder>();
+
+            var sandbox = new Sandbox(options, configurationBuilder.Object, createDirectory, deleteFiles, deleteDirectories, startProcess, literalScriptFactory);
+            await sandbox.Run(new List<IScript>()
+            {
+                // Raw script that will be executed
+                new LiteralScript(new List<string>() {
+                    @"Start-Process 'C:\windows\system32\notepad.exe'" },
+                    new PowershellTranslator(),
+                    "mockScriptName1.ps1",
+                    deleteFiles,
+                    writeAllBytes),
+
+                // Script that will open the browser
+                new ExternalScript(
+                    new MockFileSystemInfo(@"C:\Users\OpenBrowserScript.ps1"),
+                    new PowershellTranslator(),
+                    copyFiles),
+
+                // Script that will open explorer
+                new ExternalScript(
+                    new MockFileSystemInfo(@"C:\Users\OpenExplorerScript.ps1"),
+                    new PowershellTranslator(),
+                    copyFiles),
+
+                // Script that will install curl and fiddler using scoop package manager
+                new ScoopPackageManagerScript(
+                    new List<string>(){ "curl", "fiddler" },
+                    new PowershellTranslator(),
+                    "mockScriptName2",
+                    new StringBuilder(),
+                    writeAllBytes),
+            });
+
+
+            configurationBuilder.Verify(c => c.Build(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once());
+            Assert.Equal(1, timesCalled);
         }
     }
 }
