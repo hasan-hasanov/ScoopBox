e4251d80671ae33121eb7947a7f822d228c9e80c
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-11-22 03:07:54 +0200

    Add chocolatey package manager tests

---
+++ src/ScoopBox.Test/Scripts/PackageManager/Chocolatey/ChocolateyPackageManagerScriptTests/CtorTests.cs
@@ -0,0 +1,96 @@
+﻿using ScoopBox.Scripts.PackageManagers;
+using ScoopBox.Scripts.PackageManagers.Chocolatey;
+using ScoopBox.Translators;
+using ScoopBox.Translators.Powershell;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ScoopBox.Test.Scripts.PackageManager.Chocolatey.ChocolateyPackageManagerScriptTests
+{
+    public class CtorTests
+    {
+        [Fact]
+        public void ShouldInitializeWithoutTranslator()
+        {
+            IEnumerable<string> applications = new List<string>() { "git", "curl", "fiddler" };
+
+            IPackageManagerScript chocolateyPackageManager = new ChocolateyPackageManagerScript(applications);
+
+            string expected = JsonSerializer.Serialize(applications);
+            string actual = JsonSerializer.Serialize(chocolateyPackageManager.Applications);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldInitializeApplications()
+        {
+            IEnumerable<string> applications = new List<string>() { "git", "curl", "fiddler" };
+
+            IPackageManagerScript chocolateyPackageManager = new ChocolateyPackageManagerScript(applications, new PowershellTranslator());
+
+            string expected = JsonSerializer.Serialize(applications);
+            string actual = JsonSerializer.Serialize(chocolateyPackageManager.Applications);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldInitializeTranslator()
+        {
+            IEnumerable<string> applications = new List<string>() { "git", "curl", "fiddler" };
+            IPowershellTranslator powershellTranslator = new PowershellTranslator();
+
+            IPackageManagerScript chocolateyPackageManager = new ChocolateyPackageManagerScript(applications, powershellTranslator);
+
+            var expected = powershellTranslator;
+            var actual = chocolateyPackageManager.Translator;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithoutApplications()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ChocolateyPackageManagerScript(null, new PowershellTranslator()));
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithoutTranslator()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ChocolateyPackageManagerScript(new List<string>() { "git", "curl", "fiddler" }, null));
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithoutScriptName()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ChocolateyPackageManagerScript(new List<string>() { "git", "curl", "fiddler" }, new PowershellTranslator(), null));
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithoutSbChocolateyPackageManagerBuilder()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ChocolateyPackageManagerScript(
+                new List<string>() { "git", "curl", "fiddler" },
+                new PowershellTranslator(),
+                $"{nameof(ChocolateyPackageManagerScript)}.ps1",
+                null,
+                (path, content, token) => Task.CompletedTask));
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWithoutWriteAllBytesAsync()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ChocolateyPackageManagerScript(
+                new List<string>() { "git", "curl", "fiddler" },
+                new PowershellTranslator(),
+                $"{nameof(ChocolateyPackageManagerScript)}.ps1",
+                new StringBuilder(),
+                null));
+        }
+    }
+}
---
+++ src/ScoopBox.Test/Scripts/PackageManager/Chocolatey/ChocolateyPackageManagerScriptTests/ProcessMethodTests.cs
@@ -0,0 +1,76 @@
+﻿using ScoopBox.Scripts.PackageManagers.Chocolatey;
+using ScoopBox.Translators.Powershell;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ScoopBox.Test.Scripts.PackageManager.Chocolatey.ChocolateyPackageManagerScriptTests
+{
+    public class ProcessMethodTests
+    {
+        [Fact]
+        public async Task ShouldGenerateCorrectPackageManagerScriptContent()
+        {
+            IOptions options = new Options();
+
+            IEnumerable<string> applications = new List<string>() { "git", "curl" };
+            StringBuilder sbChocolateyPackageManagerBuilder = new StringBuilder();
+            Func<string, byte[], CancellationToken, Task> writeAllBytesAsync = (file, content, token) =>
+            {
+                return Task.CompletedTask;
+            };
+
+            ChocolateyPackageManagerScript chocolateyPackageManager = new ChocolateyPackageManagerScript(
+                applications,
+                new PowershellTranslator(),
+                "testScript.ps1",
+                sbChocolateyPackageManagerBuilder,
+                writeAllBytesAsync);
+
+            await chocolateyPackageManager.Process(options, CancellationToken.None);
+
+            string expected = @"Write-Host Start executing chocolatey package manager
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+choco feature enable -n allowGlobalConfirmation
+choco install git curl
+Write-Host Finished executing chocolatey package manager
+";
+            string actual = sbChocolateyPackageManagerBuilder.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task ShouldGenerateCorrectPackageManagerScriptPath()
+        {
+            IOptions options = new Options();
+            string scriptName = "testScript.ps1";
+            string actual = string.Empty;
+
+            IEnumerable<string> applications = new List<string>() { "git", "curl" };
+            StringBuilder sbChocolateyPackageManagerBuilder = new StringBuilder();
+            Func<string, byte[], CancellationToken, Task> writeAllBytesAsync = (file, content, token) =>
+            {
+                actual = file;
+                return Task.CompletedTask;
+            };
+
+            ChocolateyPackageManagerScript chocolateyPackageManager = new ChocolateyPackageManagerScript(
+                applications,
+                new PowershellTranslator(),
+                scriptName,
+                sbChocolateyPackageManagerBuilder,
+                writeAllBytesAsync);
+
+            await chocolateyPackageManager.Process(options, CancellationToken.None);
+
+            string expected = Path.Combine(options.RootFilesDirectoryLocation, scriptName);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
